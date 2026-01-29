f32c32ba542c0041cded3283386c076c7f99abf7
Théophile Schenker <theophile@MacBook-Pro-de-Theophile.local>
2026-01-29 23:12:23 +0100

    add js assets to vite.confit.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/contacts/contact-form.js',
+                'resources/js/custom-fields/custom-field-form.js',
+                'resources/js/owners/owner-form.js',
+                'resources/js/reservations/reservation-form.js',
+                'resources/js/rooms/room-form.js',
+                'resources/js/system-settings/system-settings-form.js',
+                'resources/js/config-test.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
