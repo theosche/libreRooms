a9d1071f68583ccd5d33d53f9ef71f106297bab2
Théophile Schenker <theophile@macbook-pro-de-theophile-1.home>
2026-02-17 16:42:47 +0100

    Add logo icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,10 @@
 <body>
 <header class="header">
     <nav class="nav @auth nav-authenticated @endauth">
-        <a href="/" class="nav-logo"><img src="/images/logo-text.png" width="180px"></a>
+        <a href="/" class="nav-logo">
+            <img src="/images/logo-icon.png" width="50px">
+            <img src="/images/logo-text.png" width="130px">
+        </a>
 
         @auth
             <button type="button" class="nav-toggle" onclick="toggleNavMenu()" aria-label="{{ __('Menu') }}">
